cae425b9d49f446468c7bd5f9c22304d9c14c0e0
adeel <adeelmanzoor13@gmail.com>
2022-04-18 12:34:48 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,99 @@
+let go_sign_up = document.querySelector(".signUp");
+let go_login = document.querySelector(".Login");
+
+let sign_up = document.querySelector(".sign_up");
+let login = document.querySelector(".login");
+
+let Fname = document.querySelector(".Fname");
+let Lname = document.querySelector(".Lname");
+let eemail = document.querySelector(".email");
+let ppassword = document.querySelector(".pass");
+let aage = document.querySelector(".age");
+let ccontact = document.querySelector(".contact");
+
+let activeName = document.querySelector("#activeUser");
+let logout = document.querySelector("#remove");
+
+go_sign_up?.addEventListener("click", () => {
+  window.location = "http://127.0.0.1:5500/sign_up.html";
+});
+
+go_login?.addEventListener("click", function () {
+  window.location = "http://127.0.0.1:5500/login.html";
+});
+
+let users = JSON.parse(localStorage.getItem("users")) || [];
+
+function saveData(e) {
+  let fFname = Fname.value;
+  let lLname = Lname.value;
+  let eeemail = eemail.value;
+  let pppassword = ppassword.value;
+
+  let aaage = aage.value;
+  let cccontact = ccontact.value;
+
+  if (!eeemail || !pppassword || !fFname || !lLname) {
+    alert("Please Fill All Required Fields");
+    return false;
+  } else {
+    users.push({
+      email: eeemail,
+      password: pppassword,
+      FirstName: fFname,
+      LastName: lLname,
+      age: aaage,
+      contact: cccontact,
+      todos: [],
+      // g1: () => {
+      //   document.querySelector("input[name=gender]:checked");
+      //   g1 = g1 ? g1.value : "";
+      // localStorage.setItem("g1", g1);
+      // },
+    });
+
+    localStorage.setItem("users", JSON.stringify(users));
+
+    // toast
+
+    var toastElList = [].slice.call(document.querySelectorAll(".toast"));
+    var toastList = toastElList.map(function (toastEl) {
+      return new bootstrap.Toast(toastEl);
+    });
+
+    toastList.forEach((toast) => toast.show());
+    document.querySelector("form").reset();
+  }
+  e.preventDefault();
+}
+function getData(e) {
+  e.preventDefault();
+  let users = JSON.parse(localStorage.getItem("users"));
+  let activeUser = users.find((u) => u.email === eemail.value);
+
+  // console.log(activeUser.todos[0]);
+
+  if (activeUser) {
+    if (activeUser.password === ppassword.value) {
+      // alert("login"); //2
+      localStorage.setItem("activeUser", JSON.stringify(activeUser)); //3
+      setTimeout(function () {
+        window.location.href = "dashboard.html";
+      });
+    } else {
+      alert("Wrong email or password");
+    }
+  } else {
+    alert("User deos not exist.");
+  }
+}
+sign_up?.addEventListener("click", saveData);
+login?.addEventListener("click", getData);
+
+// var activeUser = JSON.parse(localStorage.getItem("activeUser"));
+// console.log(
+//   "FirstName LastName"
+//     .split(" ")
+//     .map((n) => n[0])
+//     .join("")
+// );
